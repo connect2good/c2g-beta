b7d2b2d2f837f4869045a099066b09ba0ddcd9bc
Todd Wacker <twac05@yahoo.com>
2014-04-08 08:24:54 -0500

    List needs helper method update

--- app/helpers/needs_helper.rb
+++ app/helpers/needs_helper.rb
@@ -1,7 +1,8 @@
 module NeedsHelper
   def list_needs
-      @needs.each do |x|
-        concat content_tag :h5, "Organization #{x.organization_id}"
+    concat content_tag :h2, "Needs"
+      Need.all.each do |x|
+        concat content_tag :h4, "#{Organization.find(x.organization_id).name}"
         concat content_tag :p, "#{x.description}"
         concat button_tag "Donate"
     end
