44f961050fcf2d8bc63f4a15bae71891de87252a
Sonny Gonzalez <sonnyg.email@gmail.com>
2014-04-08 15:10:40 -0500

    add user model and devise

--- spec/support/blueprints.rb
+++ spec/support/blueprints.rb
@@ -1,45 +1,5 @@
 require 'machinist/active_record'
 require 'faker'
 
-Need.blueprint do
-  description do
-    "A book titled #{Faker::Company::bs}"
-  end
-end
-
-Offer.blueprint do
-  description { %w[books computers chairs].sample }
-  image do
-    Rack::Test::UploadedFile.new(
-      Rails.root.join('spec', 'support', 'austin-skyline-small.jpg'),
-      'image/jpg'
-    )
-  end
-  status do
-    %w[pending accepted rejected].sample
-  end
-end
-
-Organization.blueprint do
-  description { Faker::Company::catch_phrase }
-  image do
-    Rack::Test::UploadedFile.new(
-      Rails.root.join('spec', 'support', 'austin-skyline-small.jpg'),
-      'image/jpg'
-    )
-  end
-  name        { Faker::Company::name }
-  street      { '716 Congress Ave' }
-  city        { 'Austin' }
-  state       { 'TX' }
-  zip         { '78704' }
-end
-
 User.blueprint do
-  email    { Faker::Internet::email }
-  name     { Faker::Name::name }
-  password { 'test1234' }
-  password_confirmation do
-    object.password
-  end
 end
