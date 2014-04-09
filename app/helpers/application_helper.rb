a32f2c97fedcdbfe193e95ae63f2f2e44c9d7a1d
Sonny Gonzalez <sonnyg.email@gmail.com>
2014-04-09 01:45:10 -0500

    fix btn, style header and footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,49 @@
 module ApplicationHelper
+
+  def l( name='', opts={}, &block )
+    if block_given?
+      content = capture_haml { yield }
+    else
+      content = nil
+    end
+
+    render partial: 'layouts/partials/layout',
+      locals: {
+        _opts:    normalize_opts(name, opts),
+        _content: content
+      }
+  end
+
+  alias_method :m, :l
+
+  def link_btn( url='#', opts={}, &block )
+    partial 'partials/link_btn',
+      locals: {
+        _url:     url,
+        _opts:    normalize_opts('btn link-btn', opts),
+        _content: capture_haml { yield }
+      }    
+  end
+
+  def row( name='', opts={}, &block )
+    if block_given?
+      content = capture_haml { yield }
+    else
+      content = nil
+    end
+
+    render partial: 'layouts/partials/row',
+      locals: {
+        _opts:    normalize_opts(name, opts),
+        _content: content
+      }
+  end
+
+  def normalize_opts( name='', opts={} )
+    normalized_class =  name.to_s.gsub(/_/,'-') + ' '
+    normalized_class << opts[:class].to_s
+    opts[:class] = normalized_class.strip
+    opts
+  end
+
 end
