327f0265c521744913fba07897ed4e9dc1186718
Matt Denton <geositta@gmail.com>
2014-04-12 17:13:45 -0500

    Add pg-search to Models

--- app/models/merchandise.rb
+++ app/models/merchandise.rb
@@ -11,4 +11,16 @@ class Merchandise < ActiveRecord::Base
     class_name: 'Individual',
     foreign_key: 'individual_id'
 
+  include PgSearch
+  pg_search_scope :search, against: [:title, :description], 
+    using: {tsearch: {dictionary: "english"}}
+      # associated_against: {organization: [:name]}
+  def self.text_search(query)
+    if query.present?
+      search(query)
+    else
+      scoped
+    end
+  end
+
 end
--- app/models/need.rb
+++ app/models/need.rb
@@ -2,4 +2,16 @@ class Need < ActiveRecord::Base
   belongs_to :organization
   has_many :donations
 
+  include PgSearch
+  pg_search_scope :search, against: [:title, :description], 
+    using: {tsearch: {dictionary: "english"}}, 
+    associated_against: {organization: [:name]}
+  def self.text_search(query)
+    if query.present?
+      search(query)
+    else
+      scoped
+    end
+  end
+
 end
