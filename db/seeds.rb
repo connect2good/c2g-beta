74184ec9674324b713b98bc5ea2a0ee8eb642424
Sonny Gonzalez <sonnyg.email@gmail.com>
2014-04-14 13:52:29 -0500

    add admins and individuals

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,82 @@
 require Rails.root.join('spec', 'support', 'blueprints')
 require 'faker'
 
-# # Make Sonny !Admin
-# individual = Individual.make
-# individual.name = 'Sonny !Admin'
-# individual.email = 'sonnyg.email@gmail.com'
-# individual.password = 'test1234'
-# individual.password_confirmation = individual.password
-# individual.save! validate: false
+def devs_admins
+  # Make Dev Users Admin
+  [
+    { name: 'Sonny', email: 'sonny.admin@c2g.com' },
+    { name: 'Todd',  email: 'todd.admin@c2g.com'  },
+    { name: 'Matt',  email: 'matt.admin@c2g.com ' },
+    { name: 'Bill',  email: 'bill.admin@c2g.com'  },
+  ]
+end
 
-# # Make Sonny Admin
-# individual = Individual.make
-# individual.name = 'Sonny Admin'
-# individual.email = 'sg@essgee.io'
-# individual.password = 'test1234'
-# individual.password_confirmation = individual.password
-# individual.admin = true
-# individual.save! validate: false
+def devs_not_admin
+  # Make Dev Users !Admin
+  [
+    { name: 'Sonny', email: 'sonny@c2g.com' },
+    { name: 'Todd',  email: 'todd@c2g.com'  },
+    { name: 'Matt',  email: 'matt@c2g.com'  },
+    { name: 'Bill',  email: 'bill@c2g.com'  },
+  ]
+end
+
+# Parameters
+PASSWORD                     = 'test1234'
+N_INDIVIDUALS                = 3
+N_ORGANIZATIONS              = 3
+N_NEEDS_PER_ORGANIZATION     = 3
+N_MERCHANDISE_PER_INDIVIDUAL = 3
 
 
+puts 'Creating Organizations with Needs...'
 # Create Organizatons with Needs
-Organization.make!(5).each do |organization|
-  Need.make(10).each do |need|
+Organization.make!(N_ORGANIZATIONS).each do |organization|
+  Need.make(N_NEEDS_PER_ORGANIZATION).each do |need|
     need.organization_id = organization.id
     need.save! validate: false
   end
 end
 
+
+puts 'Creating Devs(Admin)...'
+# Make Dev Admins
+devs_admins.each do |admin|
+  individual = Individual.make(
+    name:     admin[:name],
+    email:    admin[:email],
+    password: PASSWORD,
+    password_confirmation: PASSWORD,
+    admin:    true
+  )
+  individual.save! validate: false
+end
+
+
+puts 'Creating Devs(!Admin) with Merchandise...'
+# Make Devs !Admin
+devs_not_admin.each do |dev|
+  individual = Individual.make(
+    name:     dev[:name],
+    email:    dev[:email],
+    password: PASSWORD,
+    password_confirmation: PASSWORD
+  )
+
+  individual.save! validate: false
+
+  # Give Dev !Admins some merchandise
+  Merchandise.make(N_MERCHANDISE_PER_INDIVIDUAL).each do |merchandise|
+    merchandise.individual_id   = individual.id
+    merchandise.organization_id = Organization.all.sample.id
+    merchandise.save! validate: false
+  end
+end
+
+puts 'Creating Individuals with Merchandise...'
 # Create Individuals with sale_itmes
-Individual.make!(5).each do |individual|
-  Merchandise.make(10).each do |merchandise|
+Individual.make!(N_INDIVIDUALS).each do |individual|
+  Merchandise.make(N_MERCHANDISE_PER_INDIVIDUAL).each do |merchandise|
     merchandise.individual_id = individual.id
     merchandise.organization_id = Organization.all.sample.id
     merchandise.save! validate: false
@@ -37,40 +84,42 @@ Individual.make!(5).each do |individual|
 end
 
 
-# Market Transactions
-Purchase.make!(5).each do |purchase|
-  # beneficiary
-  beneficiary = Organization.all.sample
+# # Market Transactions
+# Purchase.make!(5).each do |purchase|
+#   # beneficiary
+#   beneficiary = Organization.all.sample
 
-  # seller
-  merchandise = Merchandise.all.sample
-  seller      = merchandise.seller
+#   # seller
+#   merchandise = Merchandise.all.sample
+#   seller      = merchandise.seller
 
-  purchase.sale_item = merchandise
+#   purchase.sale_item = merchandise
 
-  # buyer
-  begin
-    buyer = Individual.all.sample
-  end until buyer.id != seller.id
+#   # buyer
+#   begin
+#     buyer = Individual.all.sample
+#   end until buyer.id != seller.id
 
-  purchase.buyer = buyer
-  purchase.save! validate: false
-end
+#   purchase.buyer = buyer
+#   purchase.save! validate: false
+# end
 
 
-# Donation Transaction
-Donation.make!(5).each do |donation|
-  # need
-  need = Need.all.sample
-  donation.need_id = need.id
+# # Donation Transaction
+# Donation.make!(5).each do |donation|
+#   # need
+#   need = Need.all.sample
+#   donation.need_id = need.id
 
-  # beneficiary
-  beneficiary = need.organization
+#   # beneficiary
+#   beneficiary = need.organization
 
-  # donor
-  donor = Individual.all.sample
+#   # donor
+#   donor = Individual.all.sample
 
-  donation.donor = donor
+#   donation.donor = donor
 
-  donation.save! validate: false
-end
+#   donation.save! validate: false
+# end
+
+puts "Seeds complete."
