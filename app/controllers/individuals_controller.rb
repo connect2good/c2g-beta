97bf424f62f67d530325bceda19bef978b02b76f
Sonny Gonzalez <sonnyg.email@gmail.com>
2014-04-15 10:00:21 -0500

    add dashboard layout for individual (not admin

--- app/controllers/individuals_controller.rb
+++ app/controllers/individuals_controller.rb
@@ -1,6 +1,11 @@
 class IndividualsController < ApplicationController
-  layout 'dashboard'
-  def show
+  layout 'dashboard', only: [:show]
 
+  def index
+    @individuals = Individual.all
+  end
+
+  def show
+    @individual = Individual.find_by id: params[:id]
   end
 end
