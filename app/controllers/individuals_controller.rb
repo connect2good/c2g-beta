81423ecd0432cdde74ddcc58101aa0e64445d1d8
Sonny Gonzalez <sonnyg.email@gmail.com>
2014-04-15 15:42:23 -0500

    add donations, sale_items, and purchases to individual dashboard content

--- app/controllers/individuals_controller.rb
+++ app/controllers/individuals_controller.rb
@@ -7,9 +7,10 @@ class IndividualsController < ApplicationController
 
   def show
     @individual = Individual.find_by id: params[:id]
-    @view = params[:content_view]
+    @view = @_env['PATH_INFO'].split(/\//)[-1]
     @sale_items = @individual.sale_items
     @donations  = @individual.donations
     @purchases  = @individual.purchases
+    # raise
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 C2g::Application.routes.draw do
+  # The priority is based upon order of creation:
+  # first created -> highest priority.
+
   get "inquiries/new"
   get "inquiries/create"
   get "purchases/index"
   get "purchases/new"
   get "purchases/create"
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
 
   devise_for :organizations
   devise_for :individuals
@@ -22,7 +23,18 @@ C2g::Application.routes.draw do
   end
 
   resources :individuals
-  get '/individuals/:id/:content_view', to: 'individuals#show', as: :individuals_content_view
+
+  get '/individuals/:id/sale_items',
+    to: 'individuals#show',
+    as: :individuals_sale_items
+
+  get '/individuals/:id/donations',
+    to: 'individuals#show',
+    as: :individuals_purchases
+
+  get '/individuals/:id/purchases',
+    to: 'individuals#show',
+    as: :individuals_donations
 
   resources :donations
   resources :needs
@@ -35,30 +47,5 @@ C2g::Application.routes.draw do
   namespace :admin do
     resources :individuals
   end
-
-  # get "merchandise/search"
-  # get "merchandise/index"
-  # get "merchandise/sell"
-  # get "merchandise/buy"
-  # get "merchandise/new"
-  # get "merchandise/edit"
-  # get "merchandise/destroy"
-  # get "nonprofit/new"
-  # get "needs/index"
-  # get "needs/donate"
-  # get "needs/search"
-  # get "needs/new"
-  # get "needs/edit"
-  # get "needs/destroy"
-
-  # get "donations/new"
-  # get "contact_form/new"
-  # get "contact_form/create"
-
-
-
-  # resources :search_suggestions
-
-
 
 end
