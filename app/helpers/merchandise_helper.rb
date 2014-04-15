1ab9130d5d992dd1a82f95632e80b72349859c43
Bill Fienberg <billfienberg@gmail.com>
2014-04-15 11:13:16 -0500

    Remove contact seller button from merchandises#index, and add contact seller button to sold method in merchandise_helper.rb

--- app/helpers/merchandise_helper.rb
+++ app/helpers/merchandise_helper.rb
@@ -3,7 +3,9 @@ module MerchandiseHelper
     if item.sold
       "Sold"
     else 
-      link_to "Buy", new_merchandise_purchase_path(item), class: "btn"
+      content_tag(:p, class: "buyer_options") do
+        link_to("Contact Seller", new_merchandise_inquiry_path(item), class: "btn") + " " + link_to("Buy", new_merchandise_purchase_path(item), class: "btn")
+      end
     end
   end
 end
