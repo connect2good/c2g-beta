c5b03c21f3c3c25f5370f976ee82c39e4121859e
Matt Denton <geositta@gmail.com>
2014-05-02 22:34:22 -0500

    add initialize function to purchase model, and add balanced api key to initializer

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,3 +1,5 @@
+require 'balanced'
+
 class Purchase < ActiveRecord::Base
   belongs_to :sale_item,
     class_name: 'Merchandise',
@@ -8,4 +10,15 @@ class Purchase < ActiveRecord::Base
     foreign_key: 'buyer_id'
 
     # validates :agree, presence: :true
+
+  def initialize(email, amount, credit_card_hash)
+    @email = email
+    @amount = (amount * 100).to_i
+    @credit_card_hash = credit_card_hash
+    @buyer = nil
+    @card = nil
+    @errors = []
+  end
+
+
 end
---
+++ config/initializers/balanced.rb
@@ -0,0 +1,2 @@
+require 'balanced'
+Balanced.configure(ak-test-1h7ErAXqMyjGsvdDqMx6Si3Fnu3IHzFIQ)
